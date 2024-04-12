5e271413ea8d8542969975b7223dc1c85053cb67
unknown <kimdogyun0906@gmail.com>
2024-04-12 11:04:05 +0900

    탕추가시도

--- main.py
+++ main.py
@@ -11,24 +11,41 @@ pygame.display.set_caption("PyPhysic")
 
 class Box:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.s = [x,y]
+        self.v = [0,0]
     def move(self):
         force = [0,0]
+        force[1] += 1
+        self.v[0] += force[0]
+        self.v[1] += force[1]
+        self.s[0] += self.v[0]
+        self.s[1] += self.v[1]
     def draw(self):
-        pygame.draw.rect(screen, (200,200,200), [self.x - 30, self.y - 30, 30, 30], 3)
+        pygame.draw.rect(screen, (200,200,200), [self.s[0] - 30, self.s[1] - 30, 30, 30], 3)
+
+class Ground:
+    def __init__(self, m, y):
+        self.m = m
+        self.y = y
+    def draw(self):
+        pygame.draw.rect(screen, (255, 0, 255), [0,self.y], [100, self.y + 100 * self.m], 2)
 
 
 #이벤트 루프
 running = True #게임 진행 여부에 대한 변수 True : 게임 진행 중
+clock = pygame.time.Clock()
 Boxs = [Box(100,100)]
+Grounds = [Ground(500,0)]
+
 while running:
     for event in pygame.event.get(): #이벤트의 발생 여부에 따른 반복문
         if event.type == pygame.QUIT: #창을 닫는 이벤트 발생했는가?
             running = False
 
+    clock.tick(60)
     screen.fill((0, 0, 0))
     for b in Boxs:
+        b.move()
         b.draw()
 
     #screen.blit(background, (0, 0)) #배경에 이미지 그려주고 위치 지정
