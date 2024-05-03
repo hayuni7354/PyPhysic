5f775f8854e67fd7bfb3830e735150928593be61
unknown <kimdogyun0906@gmail.com>
2024-05-03 11:03:27 +0900

    이상하게 튕기는 문제 해결, 두 평면에 접하는 경우도 추가

--- main.py
+++ main.py
@@ -22,42 +22,38 @@ class Box:
     def move(self):
         force = np.array([0.0,0.0])
         force[1] += 0.0001
-        m = self.checkGround(grounds)
+        ms= self.checkGround(grounds)
         if(self.onGround):
-            if(m != 0):
-                sint = m / ((1 + m**2)**1/2)
-                print(sint)
-            else:
-                sint = 0
-            cost = 1 / ((1 + m**2)**1/2)
-            rotate = np.array([[cost, -sint],[sint, cost]])
-            tempf = np.matmul(rotate, force)
-            tempv = np.matmul(rotate, self.v)
-            tempf[1] = 0
-            tempv[1] *= -0.5
-            rotate = np.array([[cost, sint],[-sint, cost]])
-            tempf = np.matmul(rotate, tempf)
-            tempv = np.matmul(rotate, tempv)
-            force = tempf
-            self.v = tempv
+            for m in ms: #벽이 여러개여도 잘 작동하게...
+                sint = m / ((1 + m**2)**(1/2)) #cot를 이용하는 대신 tan만 이용하는 변환을 찾음
+                cost = 1 / ((1 + m**2)**(1/2))
+                rotate = np.array([[cost, -sint],[sint, cost]])
+                tempf = np.matmul(rotate, force)
+                tempv = np.matmul(rotate, self.v)
+                tempf[1] = 0
+                tempv[1] *= -0.5
+                rotate = np.array([[cost, sint],[-sint, cost]])
+                tempf = np.matmul(rotate, tempf)
+                tempv = np.matmul(rotate, tempv)
+                force = tempf
+                self.v = tempv
         self.v[0] += force[0]
         self.v[1] += force[1]
         self.s[0] += self.v[0]
         self.s[1] += self.v[1]
     def draw(self):
         #pygame.draw.rect(screen, (200,200,200), [self.s[0] - 15, self.s[1] - 15, 30, 30], 3)
-        pygame.draw.circle(screen, (200,200,200), [self.s[0] - 15, self.s[1]], 15, 3)
+        pygame.draw.circle(screen, (200,200,200), [self.s[0], self.s[1]], 16, 3) #원의 좌표 = 원의 중심이 되도록
     def checkGround(self, gs):
         self.onGround = False
+        ms = []
         for g in gs:
-            if(g.m != 0):
-                sint = g.m / ((1 + g.m**2)**1/2)
-            else:
-                sint = 0
-            cost = 1 / ((1 + g.m**2)**1/2)
-            if(abs((self.s[1] + 15*cost) - (g.y - g.m * self.s[0])) < cost and not b.onGround):
+            sint = g.m / ((1 + g.m**2)**(1/2))
+            cost = 1 / ((1 + g.m**2)**(1/2))
+            if(abs((self.s[1] + 16/cost) - (g.y - g.m * self.s[0])) < 2*cost):
                 self.onGround = True
-                return g.m
+                ms.append(g.m)
+        return ms
                 
 
 
@@ -70,7 +66,7 @@ class Ground:
 
 
 
-class Scene1: #제미니 사용
+class Scene1: #제미니 사용, 수평면
     def __init__(self):
         global boxs
         global grounds
@@ -79,20 +75,21 @@ class Scene1: #제미니 사용
         self.delay = 0
         self.obcount = 0
         pygame.display.flip()
+        self.tick = 0
 
     def observe(self):
         for b in boxs:
             if(self.delay > 0):
                self.delay  -= 1
             elif(abs(b.v[1]) < 0.001):
-                print(484 - b.s[1])
+                print(484 - b.s[1], self.tick)
                 self.obcount += 1
                 self.delay = 1000
                 if(self.obcount >= 5):
                     global game
                     game = Scene2()
 
-class Scene2:
+class Scene2: #빗면
     def __init__(self):
         global boxs
         global grounds
@@ -101,25 +98,49 @@ class Scene2:
         self.delay = 1
         self.obcount = 0
         pygame.display.flip()
+        self.tick = 0
 
     def observe(self):
         for b in boxs:
             if(self.delay > 0):
                self.delay  -= 1
             elif(abs(b.v[1]) < 0.001):
-                print(484 - b.s[1])
+                print(484 - b.s[1], self.tick)
+                self.obcount += 1
+                self.delay = 1000
+                if(self.obcount >= 3):
+                    global game
+                    game = Scene3()
+
+class Scene3: #두 빗면
+    def __init__(self):
+        global boxs
+        global grounds
+        boxs = [Box(400,-16)]
+        grounds = [Ground(1/2, 500), Ground(-1/2, 250)]
+        self.delay = 1
+        self.obcount = 0
+        pygame.display.flip()
+        self.tick = 0
+
+    def observe(self):
+        for b in boxs:
+            if(self.delay > 0):
+               self.delay  -= 1
+            elif(abs(b.v[1]) < 0.001):
+                #print(484 - b.s[1], self.tick)
                 self.obcount += 1
                 self.delay = 1000
                 if(self.obcount >= 5):
                     global game
-                    game = Scene2()
+                    game = Scene1()
 
 #이벤트 루프
 running = True #게임 진행 여부에 대한 변수 True : 게임 진행 중
 clock = pygame.time.Clock()
 boxs = []
 grounds = []
-game = Scene2()
+game = Scene1()
 
 while running:
     for event in pygame.event.get(): #이벤트의 발생 여부에 따른 반복문
@@ -127,6 +148,7 @@ while running:
             running = False
 
     clock.tick(6000)
+    game.tick += 1
     screen.fill((0, 0, 0))
     for g in grounds:
         g.draw()
